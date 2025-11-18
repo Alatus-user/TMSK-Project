ba9c2f43ee67124830b1e7fb04f5c0efc23a4e3b
Alatus-user <thirasak26679@gmail.com>
2025-11-18 18:23:13 +0700

    Add-Cave_Monster-Scripts

--- scripts/cave_monster/CaveEntered.gd
+++ scripts/cave_monster/CaveEntered.gd
@@ -3,27 +3,31 @@ extends Area2D
 @onready var canvas_modulate: CanvasModulate = $"../CanvasModulate"
 @onready var point_light_2d: PointLight2D = $"../Player/PointLight2D2"
 
-# ค่าเริ่มต้นก่อนจะเดินเข้า
-var isCanvasDark := false
-var isLightOn := false
+const COLOR_LIGHT = Color(1, 1, 1)         # ffffff
+const COLOR_DARK  = Color(0.16, 0.16, 0.16) # 292929
 
 func _ready() -> void:
-	canvas_modulate.visible = isCanvasDark
-	point_light_2d.visible = isLightOn
+	canvas_modulate.color = COLOR_LIGHT
+	point_light_2d.visible = false
+
+
+func fade_to_dark():
+	var t = create_tween()
+	t.tween_property(canvas_modulate, "color", COLOR_DARK, 0.6)
+
+
+func fade_to_light(on_finished: Callable):
+	var t = create_tween()
+	t.tween_property(canvas_modulate, "color", COLOR_LIGHT, 0.6)
+	t.finished.connect(on_finished)  # เรียกหลัง fade เสร็จ
+
 
 func _on_body_entered(body: Node2D) -> void:
-	# ตรวจว่าเป็นผู้เล่น (ถ้าต้องการ)
 	if body.name == "Player":
-		isCanvasDark = true
-		isLightOn = true
-		
-		canvas_modulate.visible = true
 		point_light_2d.visible = true
+		fade_to_dark()
+
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.name == "Player":
-		isCanvasDark = false
-		isLightOn = false
-
-		canvas_modulate.visible = false
-		point_light_2d.visible = false
+		fade_to_light(func(): point_light_2d.visible = false)
